b648650d50f25b603755ec06c8ff391e95f3e1cc
main.mcp_smart_tui <stas@sliday.com>
2026-08-26 19:40:01 +0200

    chore: clean generated release artifacts

--- wiki/.obsidian/plugins/obsidian-front-matter-title-plugin/main.js
+++ wiki/.obsidian/plugins/obsidian-front-matter-title-plugin/main.js
@@ -4082,7 +4082,7 @@ var en = {
       main: {
         name: "Common main template",
         desc: `Set a yaml path, which value will be used as a file title. Value must be string or numeric. Also you can use template-like path using "{{ }}".
-				Also you can use #heading to use first Heading from a file or _basename and another reserved words. 
+				Also you can use #heading to use first Heading from a file or _basename and another reserved words.
 				See Readme to find out more`
       },
       fallback: {
@@ -11987,12 +11987,12 @@ reflect-metadata/Reflect.js:
   Licensed under the Apache License, Version 2.0 (the "License"); you may not use
   this file except in compliance with the License. You may obtain a copy of the
   License at http://www.apache.org/licenses/LICENSE-2.0
-  
+
   THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
   KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
   WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
   MERCHANTABLITY OR NON-INFRINGEMENT.
-  
+
   See the Apache Version 2.0 License for specific language governing permissions
   and limitations under the License.
   ***************************************************************************** *)
